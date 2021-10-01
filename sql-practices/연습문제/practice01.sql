1bef4999cfeef0f7ae7b009c6400dc75f1aa9e55
hanjoohyung <ghd167@naver.com>
2021-10-01 20:08:27 +0900

    10/01

--- sql-practices/연습문제/practice01.sql
+++ sql-practices/연습문제/practice01.sql
@@ -16,30 +16,61 @@ order by hire_date asc;
 
 -- 문제3.
 -- 여직원과 남직원은 각 각 몇 명이나 있나요?
-select count(gender('f')) as 여직원, count(gender('m')) as 남직원 
-	from employees;
-    
+
+select gender as 성별, count(*) as '인원 수'
+	from employees
+    group by gender;
 
 -- 문제4.
 -- 현재 근무하고 있는 직원 수는 몇 명입니까? (salaries 테이블을 사용합니다.) 
 
+select count(salary) as '근무중인 직원수'
+	from salaries;
+    
 -- 문제5.
 -- 부서는 총 몇 개가 있나요? 디스 머시기랑 카운트 같이써라
+
+select distinct(count(dept_no)) as '부서 개수'
+	from departments;
 
 -- 문제6.
 -- 현재 부서 매니저는 몇 명이나 있나요?(역임 매너저는 제외) 카운트 뎁트매니저
 
+select count(dept_no) as '매니저 수'
+	from dept_manager;
+    
 -- 문제7.
 -- 전체 부서를 출력하려고 합니다. 순서는 부서이름이 긴 순서대로 출력해 보세요.
+
+select dept_name as 부서
+	from departments
+order by length(dept_name) desc;
 
 -- 문제8.
 -- 현재 급여가 120,000이상 받는 사원은 몇 명이나 있습니까?
 
+select count(salary)
+	from salaries
+    where salary >= 120000;
+
 -- 문제9. 직책 뽑고 length 사용해서 추울력
 -- 어떤 직책들이 있나요? 중복 없이 이름이 긴 순서대로 출력해 보세요.
+
+select distinct(title) as 직책
+	from titles
+order by length(title) desc;
 
 -- 문제10
 -- 현재 Enginner 직책의 사원은 총 몇 명입니까?
 
+select count(title)
+	from titles
+    where title='Engineer';
+
 -- 문제11
 -- 사번이 13250(Zeydy)인 지원이 직책 변경 상황을 시간순으로 출력해보세요.
+
+select emp_no as 사번, title as 직책, from_date as '직책 시작', to_date as '직책 종료'
+	from titles
+    where emp_no='13250'
+order by from_date asc;
